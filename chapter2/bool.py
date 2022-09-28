7110ec813613e182d1441ffddb7a3945a3661a6a
jaeyeun95 <jaeyeun95@gmail.com>
2022-09-28 10:09:19 +0900

    변수와 리스트 복사

--- chapter2/bool.py
+++ chapter2/bool.py
@@ -1,11 +1,26 @@
 # 불 자료형 -> 참과 거짓을 나타내는 자료형
-# a = True
-# b = False
-# type(a)
-# type(b)
-# 1 == 1
+a = True
+b = False
+type(a)
+type(b)
+1 == 1
 
 # while 문에서 참, 거짓 
- a = [1,2,3,4]
- while a:
-    a.pop()
+a = [1,2,3,4]
+while a:
+   a.pop()
+# 같은 걸 if 문으로
+if []:
+    print("참")
+else:
+    print("거짓")
+
+if [1,2,3]:
+    print("참")
+else:
+    print("거짓")
+    
+# 불 연산
+bool('python')
+bool('')
+
