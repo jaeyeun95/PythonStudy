e43eff8f240f982c373776dcb26931cba221fbce
jaeyeun95 <jaeyeun95@gmail.com>
2022-10-26 10:00:53 +0900

    클래스 상속과 오버라이딩

--- chapter5/constructorex.py
+++ chapter5/constructorex.py
@@ -23,3 +23,27 @@ print(a.first)
 a.add()
 a.div()
 a.mul()
+
+# 클래스의 상속 
+# a의 b제곱을 계산하는 기능 추가 pow
+class MoreFourCal(FourCal):
+    def pow(self):
+        result = self.first ** self.second
+        return result
+
+b = MoreFourCal(4,2)
+b.add()
+b.mul()
+b.pow()
+c = FourCal(4,0)
+c.div()
+# 메소드 오버라이딩
+class SafeFourCal(FourCal):
+    def div(self):
+        if self.second == 0:
+            return 0
+        else:
+            return self.first / self.second
+c = SafeFourCal(4,0)
+c.div()
+# p204 나혼자 코딩 풀기
